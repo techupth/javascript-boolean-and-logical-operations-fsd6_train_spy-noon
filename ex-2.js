94cf3bc6d9c126ec6e2b4684c00e46388f16db19
trainthunder <kitikun.po@gmail.com>
2024-05-09 14:49:43 +0700

    feat: Train Hackhour

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,19 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = 4001;
+let isWeekday = "Friday";
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false;
 
-let hasPromotion;
+let hasPromotion = (lastMonthPaidMoreThan4000 >= 4000) && ((isWeekday == "Monday")||(isWeekday == "Tuesday")||(isWeekday == "Wednesday")||(isWeekday == "Thursday")||(isWeekday == "Friday")) && hasBoughtProductFromITCategory && hasAttendedDiscountEvent || (isPlatinum) ;
 
+
+console.log(lastMonthPaidMoreThan4000);
+console.log(isWeekday);
+console.log(hasBoughtProductFromITCategory);
+console.log(hasAttendedDiscountEvent);
+console.log(isPlatinum);
 console.log(hasPromotion);
+
